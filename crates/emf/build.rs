0655d563cdb9dd51b6738d256606fadc146c0196
Rie Takahashi <megumin@megu.dev>
2025-05-02 22:23:36 +0100

    feat: use libmem /MD static builds
    mlua and tracing-tracy use /MT, so this was causing compilation errors when linking msvcrt

--- crates/emf/build.rs
+++ crates/emf/build.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::fs::read_dir;
+use std::fs::{File, read_dir};
 use std::io::{self, ErrorKind};
 use std::path::PathBuf;
 
@@ -10,6 +10,8 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 	ordinal.push("emf");
 	ordinal.push("lib.def");
 	println!("cargo:rustc-cdylib-link-arg=/DEF:{}", ordinal.display());
+
+	download_libmem()?;
 
 	Ok(())
 }
@@ -28,4 +30,46 @@ fn project_root() -> io::Result<PathBuf> {
 		ErrorKind::NotFound,
 		"Ran out of places to find Cargo.toml",
 	))
+}
+
+fn download_libmem() -> Result<(), Box<dyn std::error::Error>> {
+	use flate2::read::GzDecoder;
+	use tar::Archive;
+
+	// Taken from libmem's github repo, modified to download the static MD builds, to make dependencies happy.
+	let version = "5.0.4";
+	let os_name = env::var("CARGO_CFG_TARGET_OS").unwrap();
+	let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+	let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
+
+	let archive_name = format!(
+		"libmem-{}-{}-{}-{}-static-md",
+		version, arch, os_name, target_env
+	);
+
+	let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+	let archive_path = out_dir.join(format!("{}.tar.gz", archive_name));
+
+	let download_url =
+		format!("https://github.com/rdbo/libmem/releases/download/{version}/{archive_name}.tar.gz");
+
+	println!("Downloading {}", download_url);
+
+	if !archive_path.exists() {
+		let mut req = reqwest::blocking::get(&download_url)?;
+		let mut file = std::fs::File::create(&archive_path)?;
+		std::io::copy(&mut req, &mut file)?;
+	}
+
+	let archive_dir = out_dir.join(archive_name);
+	if !archive_dir.exists() {
+		let tar_gz = File::open(&archive_path)?;
+		let tar = GzDecoder::new(tar_gz);
+		let mut tar_archive = Archive::new(tar);
+		tar_archive.unpack(out_dir)?;
+	}
+
+	let search_path = archive_dir.join("lib").join("release");
+	println!("cargo:rustc-link-search={}", search_path.display());
+	Ok(())
 }
